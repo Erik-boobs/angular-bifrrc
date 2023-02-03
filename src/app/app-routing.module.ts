e6344624a3049084768877265bf71bff1c172963
Edme <erikdanielme@outlook.com>
2023-02-03 11:44:51 -0600

    clase01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
-  {path : '',component:AppComponent,pathMatch:"full"},
+  {path: '',component:AppComponent,pathMatch:"full"},
   {path: 'login',component:LoginComponent, pathMatch:"full"},
   {path: 'register',component:RegisterComponent, pathMatch:"full"}
 ];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: String;
+  password: String;
+  
 
+  constructor(){
+    this.email = 'Email';
+    this.password = '';
+  }
+
+  login(){
+    console.log('Email: '+ this.email);
+    console.log('Password: '+ this.password);
+  }
 }
